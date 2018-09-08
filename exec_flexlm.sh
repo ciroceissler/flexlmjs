0dc5b5bcf8a3eb624de4823c29b67f0f2c2d520d
Ciro Ceissler <ciro.ceissler@gmail.com>
2018-09-08 03:01:33 -0300

    update bash script to run lmutil

--- exec_flexlm.sh
+++ exec_flexlm.sh
@@ -2,16 +2,17 @@
 
 servers=()
 
-servers+=("27645@licenses.lsc.ic.unicamp.br,quartus + modelsim + xilinx")
+servers+=("26745@licenses.lsc.ic.unicamp.br,quartus + modelsim + xilinx")
 servers+=("5280@cadence.fee.unicamp.br,cadence (irun, genus, innovus, virtuoso, jasper, ...)")
+
+FLEXLM=
 
 case $1 in
   0)
     for i in "${servers[@]}"; do
       IFS=','; read server description <<< "${i}"
 
-      output=$(cat test_flex_0.txt | grep 'license server UP')
-      # output=$(./lmutil lmstat -c ${server} | grep 'license server UP')
+      output=$(lmutil lmstat -c ${server} | grep 'license server UP')
 
       status='down'
       [[ ! -z "${output}" ]] && status='up'
@@ -20,14 +21,12 @@ case $1 in
     done
     ;;
   1)
-    output=$(cat test_flex_1.txt | grep 'Users of\|, start')
-    # output=$(./lmutil lmstat -c $2 -a | grep 'Users of')
+    output=$(lmutil lmstat -c $2 -a | grep 'Users of\|, start')
 
     echo "${output}"
     ;;
   2)
-    output=$(cat test_flex_2.txt | grep '[0-9]\.')
-    # output=$(./lmutil lmstat -c $2 -i | grep '[0-9]\.')
+    output=$(lmutil lmstat -c $2 -i | grep '[0-9]\.')
 
     echo "${output}"
     ;;
